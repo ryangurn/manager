5904ed69fb2be05d4d984836afa64d5d43d465ac
Ryan Gurnick <ryangurnick@gmail.com>
2022-04-13 02:21:52 -0400

    Adding Project Resource & Related Code

---
+++ app/Nova/Project.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Nova;
+
+use App\Models\Project as ProjectModel;
+use App\Models\User as UserModel;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Trix;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Project extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static string $model = ProjectModel::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'name';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id', 'name', 'description'
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param NovaRequest $request
+     * @return array
+     */
+    public function fields(NovaRequest $request): array
+    {
+        return [
+            ID::make()->sortable(),
+
+            BelongsTo::make('Owner', 'owner', 'App\Nova\User')
+                ->sortable(),
+
+            Text::make('Name')
+                ->required()
+                ->sortable(),
+
+            Trix::make('Description'),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param NovaRequest $request
+     * @return array
+     */
+    public function cards(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param NovaRequest $request
+     * @return array
+     */
+    public function filters(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param NovaRequest $request
+     * @return array
+     */
+    public function lenses(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param NovaRequest $request
+     * @return array
+     */
+    public function actions(NovaRequest $request): array
+    {
+        return [];
+    }
+}
