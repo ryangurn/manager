6c4988f45aa8209b4e8b2fadde390eb01837e386
Ryan Gurnick <ryangurnick@gmail.com>
2022-04-14 11:14:13 -0400

    adding users & members relationship

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Project extends Model
 {
@@ -14,5 +16,27 @@ class Project extends Model
     public function owner(): BelongsTo
     {
         return $this->belongsTo(User::class, 'owner_id', 'id');
+    }
+
+    /**
+     * @return HasManyThrough
+     */
+    public function users(): HasManyThrough
+    {
+        return $this->hasManyThrough(
+            User::class,
+            ProjectMember::class,
+            'project_id',
+            'id',
+            'id',
+            'user_id');
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function members(): HasMany
+    {
+        return $this->hasMany(ProjectMember::class, 'project_id', 'id');
     }
 }
