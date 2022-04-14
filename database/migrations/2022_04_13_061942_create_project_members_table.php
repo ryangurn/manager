9b2e02a2c17d21ca2447a5456589ab08b3153f89
Ryan Gurnick <ryangurnick@gmail.com>
2022-04-14 11:13:36 -0400

    adding project member

---
+++ database/migrations/2022_04_13_061942_create_project_members_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_members', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Project::class)->constrained();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->text('roles')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_members');
+    }
+};
