20feb41fb3515f4dc72edcc0225ed21b99a1e346
Ryan Gurnick <ryangurnick@gmail.com>
2022-04-15 15:37:54 -0400

    adding task lists model

---
+++ app/Models/TaskList.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class TaskList extends Model
+{
+    /**
+     * @return BelongsTo
+     */
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id', 'id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class, 'project_id', 'id');
+    }
+}
---
+++ database/migrations/2022_04_15_192154_create_task_lists_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Project as ProjectModel;
+use App\Models\User as UserModel;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_lists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(ProjectModel::class)->constrained();
+            $table->foreignIdFor(UserModel::class, 'owner_id')->constrained('users', 'id');
+            $table->string('name');
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_lists');
+    }
+};
